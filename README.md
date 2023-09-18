532e1078f1afa96477b77adc6df8f75c863d8330
yachty66 <maxhager28@gmail.com>
2023-09-18 23:11:57 +0200

    changes app.py

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# demo-controlnet-canny
+![](https://www.banana.dev/lib_zOkYpJoyYVcAamDf/x2p804nk9qvjb1vg.svg?w=340 "Banana.dev")
+
+# Banana.dev sd-controlnet-canny starter template
+
+This is a sd-controlnet-canny starter template from [Banana.dev](https://www.banana.dev) that allows on-demand serverless GPU inference.
+
+You can fork this repository and deploy it on Banana as is, or customize it based on your own needs.
+
+# Running this app
+
+## Deploying on Banana.dev
+
+1. [Fork this](https://github.com/bananaml/demo-controlnet-canny/fork) repository to your own Github account.
+2. Connect your Github account on Banana.
+3. [Create a new model](https://app.banana.dev/deploy) on Banana from the forked Github repository.
+
+## Running after deploying
+
+1. Wait for the model to build after creating it.
+2. Make an API request using one of the provided snippets in your Banana dashboard. However, instead of sending a prompt as provided in the snippet, send your image url as follows:
+
+```python
+inputs = {
+    "image": "your_image_url"
+}
+```
+
+For more info, check out the [Banana.dev docs](https://docs.banana.dev/banana-docs/).
--- app.py
+++ app.py
@@ -13,11 +13,11 @@ app = Potassium("controlnet-canny")
 @app.init
 def init():
     controlnet = ControlNetModel.from_pretrained(
-        "lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16
-    )
+        "lllyasviel/sd-controlnet-canny"
+    ).to("cuda:0")
     pipe = StableDiffusionControlNetPipeline.from_pretrained(
-        "runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
-    )
+        "runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None
+    ).to("cuda:0")
     context = {
         "model": controlnet, 
         "pipe": pipe
@@ -38,11 +38,11 @@ def handler(context: dict, request: Request) -> Response:
     image = np.concatenate([image, image, image], axis=2)
     image = Image.fromarray(image)
     pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
-    image = pipe("bird", image, num_inference_steps=20).images[0]
+    image = pipe("image", image, num_inference_steps=20).images[0]
     buffered = BytesIO()
     image.save(buffered, format="PNG")
     img_str = base64.b64encode(buffered.getvalue())
-    return Response(json = {"content": img_str.decode('utf-8')}, status=200)
+    return Response(json = {"output": img_str.decode('utf-8')}, status=200)
     
 if __name__ == "__main__":
     app.serve()
